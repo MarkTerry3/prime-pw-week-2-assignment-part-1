c592c9c0354353b67a2ff2badc97b35d20032329
Mark Terry <mark.terry93@gmail.com>
2021-12-13 17:59:47 -0600

    final Push

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -3,7 +3,7 @@
 let firstName = 'Mark ';
 
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-let lastName = 'Terry';
+let lastName = 'Terry'; 
 
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
